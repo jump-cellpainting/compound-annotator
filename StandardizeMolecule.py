f92684d84157f5a8e5f9cdeae4cf9a20f4a94833
Shantanu Singh <shsingh@broadinstitute.org>
2023-04-15 06:08:26 -0400

    several updates

--- StandardizeMolecule.py
+++ StandardizeMolecule.py
@@ -19,20 +19,23 @@ class StandardizeMolecule:
         output: str = None,
         num_cpu: int = 1,
         limit_rows: int = None,
+        augment: bool = False,
     ):
         """
         Initialize the class.
 
         :param input: Input file name (TSV/TXT/CSV) or a pandas dataframe containing the SMILES
         :param output: Output file name (optional)
-        :param num_cpu: Number of CPUs to use
+        :param num_cpu: Number of CPUs to use (default: 1)
         :param limit_rows: Limit the number of rows to be processed (optional)
+        :param augment: The output is the input file augmented with the standardized SMILES, InChI, and InChIKey (default: False)
 
         """
         self.input = input
         self.output = output
         self.num_cpu = num_cpu
         self.limit_rows = limit_rows
+        self.augment = augment
 
         # Disable RDKit logging
         rdBase.DisableLog("rdApp.*")
@@ -50,7 +53,7 @@ class StandardizeMolecule:
         """
         standardizer = Standardizer()
 
-        smiles_orginal = smiles
+        smiles_original = smiles
 
         # Read SMILES and convert it to RDKit mol object
         mol = MolFromSmiles(smiles)
@@ -72,46 +75,46 @@ class StandardizeMolecule:
                 mol = standardizer.stereo_parent(mol)
                 mol = standardizer.tautomer_parent(mol)
                 mol = standardizer.standardize(mol)
-                standardized_mol = mol
+                mol_standardized = mol
 
                 # convert mol object back to SMILES
-                standardized_smiles = MolToSmiles(standardized_mol)
+                smiles_standardized = MolToSmiles(mol_standardized)
 
-                if smiles == standardized_smiles:
+                if smiles == smiles_standardized:
                     is_finalize = True
                     break
 
-                smiles_clean_counter[standardized_smiles] += 1
-                if standardized_smiles not in mol_dict:
-                    mol_dict[standardized_smiles] = standardized_mol
+                smiles_clean_counter[smiles_standardized] += 1
+                if smiles_standardized not in mol_dict:
+                    mol_dict[smiles_standardized] = mol_standardized
 
-                smiles = standardized_smiles
+                smiles = smiles_standardized
                 mol = MolFromSmiles(smiles)
 
             if not is_finalize:
                 # If the standardization process is not finalized, we choose the most common SMILES from the counter
-                standardized_smiles = smiles_clean_counter.most_common()[0][0]
+                smiles_standardized = smiles_clean_counter.most_common()[0][0]
                 # ... and the corresponding mol object
-                standardized_mol = mol_dict[standardized_smiles]
+                mol_standardized = mol_dict[smiles_standardized]
 
             # Convert the mol object to InChI
-            standardized_inchi = MolToInchi(standardized_mol)
+            inchi_standardized = MolToInchi(mol_standardized)
             # Convert the InChI to InChIKey
-            standardized_inchikey = MolToInchiKey(standardized_mol)
+            inchikey_standardized = MolToInchiKey(mol_standardized)
 
         except (ValueError, AttributeError) as e:
-            standardized_smiles = np.nan
-            standardized_inchi = np.nan
-            standardized_inchikey = np.nan
+            smiles_standardized = np.nan
+            inchi_standardized = np.nan
+            inchikey_standardized = np.nan
             logging.error(f"Standardization error, {smiles}, Error Type: {str(e)}")
 
         # return as a dataframe
         return pd.DataFrame(
             {
-                "SMILES_original": [smiles_orginal],
-                "SMILES": [standardized_smiles],
-                "InChI": [standardized_inchi],
-                "InChIKey": [standardized_inchikey],
+                "SMILES_original": [smiles_original],
+                "SMILES_standardized": [smiles_standardized],
+                "InChI_standardized": [inchi_standardized],
+                "InChIKey_standardized": [inchikey_standardized],
             }
         )
 
@@ -143,6 +146,10 @@ class StandardizeMolecule:
             response = requests.get(file_or_url)
             content = response.content.decode("utf-8")
             lines = content.splitlines()
+        elif file_or_url.endswith(".gz"):
+            import gzip
+            with gzip.open(file_or_url, "rt") as f:
+                lines = f.readlines()
         else:
             with open(file_or_url, "r") as f:
                 lines = f.readlines()
@@ -161,9 +168,9 @@ class StandardizeMolecule:
         """
         if isinstance(self.input, str):
             # read the input file, and figure out if it is a csv or a tsv file
-            if self.input.endswith(".csv"):
+            if self.input.endswith((".csv", ".csv.gz")):
                 sep = ","
-            elif self.input.endswith((".tsv", ".txt")):
+            elif self.input.endswith((".tsv", ".txt", ".tsv.gz", ".txt.gz")):
                 sep = "\t"
             else:
                 raise ValueError("Input file must be either a csv or a tsv/txt file.")
@@ -174,37 +181,33 @@ class StandardizeMolecule:
                 skiprows=self.skip_rows_bang(self.input),
             )
 
-            # if there are no rows, raise an error
-            if len(self.input) == 0:
-                raise ValueError("Input file must contain at least one row.")
-
-            # if the columns SMILES or smiles is not present, raise an error
-            if (
-                "SMILES" not in self.input.columns
-                and "smiles" not in self.input.columns
-            ):
-                raise ValueError(
-                    "Input file must contain a column named SMILES or smiles."
-                )
-
-            # if the column SMILES is not present, rename the column smiles to SMILES
-            if "SMILES" not in self.input.columns:
-                self.input = self.input.rename(columns={"smiles": "SMILES"})
-
-            # if both SMILES and smiles are present, raise an error
-            if "SMILES" in self.input.columns and "smiles" in self.input.columns:
-                raise ValueError(
-                    "Input file must contain only one column named SMILES."
-                )
-
         elif isinstance(self.input, pd.DataFrame):
             pass
         else:
             raise ValueError("Input must be either a filename or a pandas dataframe.")
 
+        # if there are no rows, raise an error
+        if len(self.input) == 0:
+            raise ValueError("Input file must contain at least one row.")
+
+        # if both SMILES and smiles are present, raise an error
+        if "SMILES" in self.input.columns and "smiles" in self.input.columns:
+            raise ValueError("Input file must contain only one column named SMILES.")
+
+        # if the columns SMILES or smiles is not present, raise an error
+        if "SMILES" not in self.input.columns and "smiles" not in self.input.columns:
+            raise ValueError("Input file must contain a column named SMILES or smiles.")
+
+        # if the column SMILES is not present, rename the column smiles to SMILES
+        if "SMILES" not in self.input.columns:
+            self.input = self.input.rename(columns={"smiles": "SMILES"})
+
         # if self.limit_rows is not None, limit the number of rows to self.limit_rows
         if self.limit_rows is not None:
             self.input = self.input.head(self.limit_rows)
+
+        # save the self.input to self.input_original
+        self.input_original = self.input.copy()
 
         # select only the SMILES column
         self.input = self.input[["SMILES"]]
@@ -221,7 +224,33 @@ class StandardizeMolecule:
         """
         self._load_input()
 
+        logging.info(f"Number of CPUs: {self.num_cpu}")
+
         standardized_df = self._run_standardize(self.input["SMILES"])
+
+        # if self.augment is True, merge the original dataframe with the standardized dataframe
+
+        if self.augment:
+            # if any of these columns are already present in the original dataframe, drop them
+            new_columns = [
+                "SMILES_original",
+                "SMILES_standardized",
+                "InChI_standardized",
+                "InChIKey_standardized",
+            ]
+            for column in new_columns:
+                if column in self.input_original.columns:
+                    self.input_original = self.input_original.drop(columns=column)
+
+            standardized_df = pd.merge(
+                standardized_df,
+                self.input_original,
+                left_on="SMILES_original",
+                right_on="SMILES",
+                how="left",
+            )
+
+            standardized_df.drop("SMILES", axis=1, inplace=True)
 
         # if self.output is not None, save the standardized dataframe to a csv file
         if self.output is not None:
